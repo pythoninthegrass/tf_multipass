09bf094a9eb6370d011d4ad6aac851122ad6b3e6
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-12-11 12:26:28 -0600

    fix: fix terraform init
    Terraform backend state.conf can't be read until first init. `terraform.tf` blocks this. Commented
    out file to work around it; likely will need to exclude from future commits

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,6 @@
-terraform {
-  # Storing the .tfstate locally.
-  # This is suitable only for testing purposes. Store it on S3 compatible backend for reliability
-  backend "local" {}
-}
+# ! Will block first run of `terraform init`; can uncomment after first run
+# terraform {
+#   # Storing the .tfstate locally.
+#   # This is suitable only for testing purposes. Store it on S3 compatible backend for reliability
+#   backend "local" {}
+# }
